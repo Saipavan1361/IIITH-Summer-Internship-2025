d223cf457ac2461012b19d42a7f52ec7e9d1ff9d
Chidumulla Saipavan Reddy <120098697+Saipavan1361@users.noreply.github.com>
2025-05-30 21:49:37 +0530

    Add files via upload

---
+++ Outlinetest.py
@@ -0,0 +1,17 @@
+from ultralytics import YOLO as Y
+
+# Load models
+m = Y('yolov8n.pt')
+m1 = Y('yolov8n-seg.pt')
+m2 = Y('yolov8s.pt')
+m3 = Y('yolov8m.pt')
+m4 = Y('yolov8x.pt')
+m5 = Y('yolov8l.pt')
+
+# Predict and save results to custom folders
+m1.predict(source="https://ultralytics.com/images/bus.jpg", save=True, name="nano-seg")
+m.predict(source="bus.jpg", save=True, name="nano")
+m2.predict(source="bus.jpg", save=True, name="small")
+m3.predict(source="bus.jpg", save=True, name="medium")
+m4.predict(source="bus.jpg", save=True, name="xlarge")
+m5.predict(source="bus.jpg", save=True, name="large")
